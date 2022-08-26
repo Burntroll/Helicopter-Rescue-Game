d57be1e92aa5771ee4f552bf769225bb8a1132d8
Burntroll <francisco.silva.ru@gmail.com>
2022-08-26 22:23:59 +0300

    Dada animaçao a elementos do jogo

--- script/script.js
+++ script/script.js
@@ -2,9 +2,9 @@ function start() {
 
 	$("#inicio").hide(); // O hide é um comando Jquery
 	
-	$("#fundoGame").append("<div id='jogador'></div>");
-	$("#fundoGame").append("<div id='inimigo1'></div>");
+	$("#fundoGame").append("<div id='jogador' class='anima1'></div>");
+	$("#fundoGame").append("<div id='inimigo1' class='anima2'></div>");
 	$("#fundoGame").append("<div id='inimigo2'></div>");
-	$("#fundoGame").append("<div id='amigo'></div>");
+	$("#fundoGame").append("<div id='amigo' class='anima3'></div>");
     // A div está sendo chamada com $ por conta do Jquery
 }
