c3ac7d93dd95d1bd5ff81d91b0496224a51af573
Burntroll <francisco.silva.ru@gmail.com>
2022-08-26 22:00:54 +0300

    Montagem do cenario do jogo

---
+++ script/script.js
@@ -0,0 +1,10 @@
+function start() {
+
+	$("#inicio").hide(); // O hide é um comando Jquery
+	
+	$("#fundoGame").append("<div id='jogador'></div>");
+	$("#fundoGame").append("<div id='inimigo1'></div>");
+	$("#fundoGame").append("<div id='inimigo2'></div>");
+	$("#fundoGame").append("<div id='amigo'></div>");
+    // A div está sendo chamada com $ por conta do Jquery
+}
